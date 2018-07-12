55807451deb11aaa00f51e2f248258a3d4a18e2c
rogercodes1 <roger.ucsb@gmail.com>
2018-07-11 22:39:39 -0400

    no messages bug found and fixed.

--- src/Messages/MessagesCont.js
+++ src/Messages/MessagesCont.js
@@ -3,7 +3,7 @@ import EventListCard from './EventListCard';
 import MessageCard from './MessageCard';
 import MessageForm from './MessageForm';
 // import Fetches from '../Fetches';
-import {Grid, Menu, Segment, Comment} from 'semantic-ui-react';
+import {Grid, Menu, Segment, Image, Header, Comment} from 'semantic-ui-react';
 import {connect} from 'react-redux';
 import {saveUserEventsAction, onChangeChatMessageAction, activeItemAction,selectedChatEventAction, eventCommentsAction} from '../actions';
 const url = `http://localhost:3001/api/v1/users/${localStorage.id}/`
@@ -19,11 +19,16 @@ fetchData = () => {
   fetch(url)
   .then(response=>response.json())
   .then(json=>{
-    let activeItem = json.events[0].id.toString()
-  this.props.updateActiveItem(activeItem)
-  this.props.saveUserEvents(json.events)
-  this.props.currentEvent(json.events[0])
-  this.props.storeComments(json.events[0].comments)
+    if (json.events.length !== 0){
+      let activeItem = json.events[0].id.toString()
+    this.props.updateActiveItem(activeItem)
+    this.props.saveUserEvents(json.events)
+    this.props.currentEvent(json.events[0])
+    this.props.storeComments(json.events[0].comments)
+  } else {
+      this.props.saveUserEvents(json.events)
+  }
+
   })
 }
 displayEventsList = (events, activeItem) => {
@@ -86,25 +91,32 @@ handleInputChange = (e) => {
     console.log(activeItem);
     return(
       <div id="MessagesCont">
-      <Grid>
-        <Grid.Column width={6} id="MessageEventsColumn">
-          <Menu fluid vertical tabular >
-            {this.displayEventsList(userEvents, activeItem)}
-          </Menu>
-        </Grid.Column>
-        <Grid.Column stretched width={10}>
-          <Segment id='MessageCardCont'>
-            <Comment.Group >
-            {this.displayEventComments(userEvents, activeItem)}
-          </Comment.Group>
-        </Segment>
-        <MessageForm
-          {...this.props}
-          handleInputChange={this.handleInputChange}
-          handleSubmit={this.handleSubmit}/>
-        </Grid.Column>
+        {(this.props.userEvents.length > 0) ?
+         (<Grid>
+            <Grid.Column width={6} id="MessageEventsColumn">
+              <Menu fluid vertical tabular >
+                {this.displayEventsList(userEvents, activeItem)}
+              </Menu>
+            </Grid.Column>
+            <Grid.Column stretched width={10}>
+              <Segment id='MessageCardCont'>
+                <Comment.Group >
+                {this.displayEventComments(userEvents, activeItem)}
+              </Comment.Group>
+            </Segment>
+            <MessageForm
+              {...this.props}
+              handleInputChange={this.handleInputChange}
+              handleSubmit={this.handleSubmit}/>
+            </Grid.Column>
 
-      </Grid>
+          </Grid>)
+          : (
+          <React.Fragment>
+            <Header size="huge">Let's go Explore events. Messaging not available yet.</Header>
+            <Image id="corgi" src="https://media.giphy.com/media/VFDeGtRSHswfe/giphy.gif"/>
+          </React.Fragment>)
+          }
 
     </div>
     )
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { withRouter } from 'react-router'
-import { Menu, Icon,Modal, Dropdown } from 'semantic-ui-react'
+import { Menu, Icon,Modal, Dropdown, Item } from 'semantic-ui-react'
 import {NavLink} from 'react-router-dom';
 // import AuthO from './AuthO'
 
@@ -14,6 +14,16 @@ class NavBar extends Component{
     }
 show = dimmer => () => this.setState({ dimmer, open: true })
 close = () => this.setState({ open: false })
+
+handleModalClick = (e, {name, dimmer}) => {
+  this.setState({
+    dimmer,
+    open: false,
+    activeItem: name,
+
+  })
+
+}
 
 handleClick = (e, { name }) => this.setState({ activeItem: name })
 
@@ -30,8 +40,8 @@ handleProfile = (e) => this.props.history.push("/profile")
     return (
       <Menu>
             <Menu.Item
-                as={NavLink}
-                to="/home"
+              as={NavLink}
+              to="/home"
               name='home'
               active={activeItem === 'home'}
               onClick={this.handleClick}>
@@ -86,6 +96,53 @@ handleProfile = (e) => this.props.history.push("/profile")
           <Modal.Header>Navigating Meet Friends</Modal.Header>
           <Modal.Content image>
 
+            <Modal.Actions>
+              <p><Menu.Item
+                as={NavLink}
+                to="/home"
+                name=' home '
+                active={activeItem === 'home'}
+                onClick={this.handleModalClick}>
+                <Icon name="home"/>Home &nbsp;
+              </Menu.Item>
+               will display all you current and past events. If no events present you can join nearby events when you scroll down.
+            </p>
+
+
+              <p><Menu.Item
+                as={NavLink}
+                to="/explore-places"
+                name='places'
+                active={activeItem === 'places'}
+                onClick={this.handleModalClick}>
+                <Icon name="yelp"/>Explore Places &nbsp;
+                </Menu.Item> lets you check out places by name or category. From there you can find details through Yelp, Google Maps, and create an event to Meet Friends.
+              </p>
+
+            <p>
+              <Menu.Item
+                  as={NavLink}
+                  to="/explore-events"
+                  name='events' active={activeItem === 'events'} onClick={this.handleModalClick}>
+                  <Icon name="searchengin"/>Explore Events &nbsp;
+              </Menu.Item>
+              shows you current events that are going on in your area. Feel free to look around and join an event. Check out the details to make sure you are all in.
+            </p>
+              <p>
+                <Menu.Item
+                  as={NavLink}
+                  to="/messages"
+                  name='messages'
+                  active={activeItem === 'messages'}
+                  onClick={this.handleModalClick}>
+                <Icon name="comments outline"/>Messages &nbsp;
+              </Menu.Item> allows you to interact with your new friends before meeting up. This way you can group chat and figure out the details on where at the event you will meet.
+            </p>
+            </Modal.Actions>
+
+
+
+
             </Modal.Content>
         </Modal>
       </Menu>
@@ -96,3 +153,23 @@ handleProfile = (e) => this.props.history.push("/profile")
 export default withRouter(NavBar);
 // add after first modal tag
   // <Image wrapped size='medium' src={props.yelp_image} />
+
+
+  // <Modal.Description icon="home">
+  //     Within your
+  //      page you will find your current and past events.
+  //   </Modal.Description>
+
+  // <Modal.Actions >
+  //   <Item.Group>
+        // <Item
+        //     as={NavLink}
+        //     to="/messages"
+        //     name='messages'
+        //     active={activeItem === 'messages'}
+        //     onClick={this.handleModalClick}>
+        //   <Icon name="comments outline"/>Messages
+        // </Item>
+  //     </Item.Group>
+  //
+  //     </Modal.Actions>
