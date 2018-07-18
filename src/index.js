1e69ddef8058fa2a01716e6d16cd5c0934f8bdf9
rogercodes1 <roger.ucsb@gmail.com>
2018-07-18 15:29:22 -0400

    moved react router dom detect.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './Assets/index.css';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore} from 'redux'
 import reducer from './reducer/reducer.js';
 import {Provider} from 'react-redux';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
   // eslint-disable no-underscore-dangle
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 // eslint-enable
