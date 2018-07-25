9051a47b3c85be63ce21f895a280342ed0b62cf4
rogercodes1 <roger.ucsb@gmail.com>
2018-07-24 22:27:19 -0400

    updated time stamp.

--- src/Messages/MessageCard.js
+++ src/Messages/MessageCard.js
@@ -7,7 +7,8 @@ class MessageCard extends Component{
 
   render(){
     const props = this.props
-
+    const {comment} = this.props
+    const timeStamp = this.props.created_at.split("T")
     return (
 
       <React.Fragment>
@@ -16,9 +17,12 @@ class MessageCard extends Component{
         <Comment.Content>
           <Comment.Author>Your Initials Here. R.P.</Comment.Author>
           <Comment.Metadata>
-            <div>Today at 5:42PM</div>
+            <div>
+              Date: {timeStamp[0]} &nbsp;
+            Time: {timeStamp[1].split("Z")[0]}
+          </div>
           </Comment.Metadata>
-          <Comment.Text>{props.comment}</Comment.Text>
+          <Comment.Text>{comment}</Comment.Text>
           <Comment.Actions>
             <Comment.Action>May not be used</Comment.Action>
           </Comment.Actions>
