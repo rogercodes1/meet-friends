0bae14980e9dd6a3cca9b04edc9c0f1fbdf54f26
rogercodes1 <roger.ucsb@gmail.com>
2018-08-08 18:07:30 -0700

    added statelens component to footer.

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Footer = (props) => {
+  return (
+    <div>
+      This is the footer.
+
+    </div>
+  )
+}
+
+export default Footer
