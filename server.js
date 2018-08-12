422e83d41972a2e183bca5f9c7aedb3056bbadd7
rogercodes1 <roger.ucsb@gmail.com>
2018-08-12 12:48:48 -0700

    added api.js to setup webSockets.

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const io = require('socket.io')();
-const port = 8000;
 
 
 
 io.on('connection', client => {
   console.log('User Connected');
-
-  // client.on('postMessage', function(data){
-  //   io.emit('updateMessages', data)
-  // });
+  client.on('subcribeToTimer', interval=>{
+    // io.emit('updateMessages', interval)
+    console.log('client is loading', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
 });
+
+const port = 8000;
 io.listen(port);
 console.log('listening on port ', port);
