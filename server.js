80bf65ef1eaf0c7f7147559b4b5cf954f7d6acb7
rogercodes1 <roger.ucsb@gmail.com>
2018-08-12 18:10:46 -0700

    added code setup for server port 8000

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
+// const io = require('socket.io')();
+
+// io.on('connection', client => {
+//   console.log('User Connected');
+//   client.on('subcribeToTimer', interval=>{
+//     // io.emit('updateMessages', interval)
+//     console.log('client is loading', interval);
+//     setInterval(() => {
+//       client.emit('timer', new Date());
+//     }, interval);
+//   });
+// });
+//
+// const port = 8000;
+// io.listen(port);
+// console.log('listening on port ', port);
+
+
+const express = require('express');
+const http = require('http');
 const io = require('socket.io')();
+const app = express()
 
+app.set('port', process.env.PORT || 8000 );
 
+let server = app.listen(app.get('port'), ()=>{
+  console.log("listening on port " + app.get('port'));
+})
+io.attach(server)
 
-io.on('connection', client => {
+io.on('connection', (socket)=> {
   console.log('User Connected');
-  client.on('subcribeToTimer', interval=>{
-    // io.emit('updateMessages', interval)
-    console.log('client is loading', interval);
-    setInterval(() => {
-      client.emit('timer', new Date());
-    }, interval);
+
+  socket.on('disconnect',()=>{
+    console.log('user diconnected');
   });
 });
-
-const port = 8000;
-io.listen(port);
-console.log('listening on port ', port);
