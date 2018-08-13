01a675ee8ca25a75c4fa1f30b69aea747023d4be
rogercodes1 <roger.ucsb@gmail.com>
2018-08-13 14:25:19 -0700

    removed express js code for live chat.

--- src/Messages/MessageForm.js
+++ src/Messages/MessageForm.js
@@ -1,49 +1,12 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux';
 import {eventCommentsAction, onChangeChatMessageAction} from '../actions';
-import {io} from './server.js';
 // import {Form, Button} from 'semantic-ui-react';
 // import Fetches from '../Fetches';
 let url =`${process.env.REACT_APP_BACKEND_URL}api/v1/comments`
-let socket = io();
 
 class MessageForm extends Component{
-let chatUsername = document.querySelector('#chat-username');
-let chatMessage = document.querySelector('#chat-message');
-socket.on('connect', function() {
-  let chatForm = document.forms.chatForm;
-
-  if (chatForm) {
-
-    chatForm.addEventListener('submit', function(e) {
-      e.preventDefault();
-
-      socket.emit('postMessage',{
-        username: chatUsername.value,
-        message: chatMessage.value,
-      });
-      chatMessage.value='';
-      chatMessage.focus();
-    }); //chat form event
-    socket.on('updateMessages',function(data) {
-      showMessage(data)
-    }); //update messages
-  } //chat form
-})
-
-function showMessage(data) {
-  let chatDisplay = document.querySelector('.chat-display');
-  let newMessage = document.createElement('p');
-  newMessage.className = 'bg-success chat-text';
-  if (chatUsername.value == data.username){
-    newMessage.className = 'bg-success chat-text'
-  } else {
-    newMessage.className = 'bg-info text-warning chat-text'
-
-  }
-  newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
-  chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
-}
+  
 
   render(){
 
