eab629c5c3449defc9de719d7ee1baff7c8ade75
rogercodes1 <roger.ucsb@gmail.com>
2018-08-18 16:36:12 -0700

    updated colors for footer.

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,9 @@ import { SocialIcon } from 'react-social-icons';
 const Footer = (props) => {
   return (
     <div id="footer">
+      <h3>Connect with Me!!</h3>
       <SocialIcon url="https://www.linkedin.com/in/rogerperez1/"/>
-      <SocialIcon url="https://github.com/rogercodes1"/>
+      <SocialIcon url="https://github.com/rogercodes1" style={`color:black`}/>
       <SocialIcon url="https://twitter.com/rogercodes1"/>
       <SocialIcon url="https://medium.com/@rogercodes1" />
     </div>
